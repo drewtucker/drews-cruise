af26a23ce2cd074462d9ec958670eb66294dc83e
dt <dtuck43@gmail.com>
2018-11-16 14:15:42 -0800

    Unignore firebaseConfig

---
+++ src/constants/firebaseConfig.js
@@ -0,0 +1,14 @@
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDfabblQw_jlylpkLyez-tw86h_ZcpDhh4",
+  authDomain: "casimir-cruises.firebaseapp.com",
+  databaseURL: "https://casimir-cruises.firebaseio.com",
+  projectId: "casimir-cruises",
+  storageBucket: "casimir-cruises.appspot.com",
+  messagingSenderId: "188866348370"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
